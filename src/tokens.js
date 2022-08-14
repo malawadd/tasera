bc941985943421cc90d91d342a8d44f4c86a3f9b
malawadd <50057882+malawadd@users.noreply.github.com>
2022-08-14 20:12:41 +0300

    💰 token List

---
+++ src/tokens.js
@@ -0,0 +1,17 @@
+export const TokenList = {
+    mumbai: [
+      {
+        symbol: "WETH",
+        address: "0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889"
+      },
+      {
+        symbol: "DAI",
+        address: "0x001B3B4d0F3714Ca98ba10F6042DaEbF0B1B7b6F"
+      },
+      {
+        symbol: "LINK",
+        address: "0x326C977E6efc84E512bB9C30f76E30c160eD06FB"
+      },
+      
+    ]
+  };
