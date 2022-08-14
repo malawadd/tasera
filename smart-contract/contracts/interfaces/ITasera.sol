412d4b3eefadccc4244ad73ae52b9401591af1ba
malawadd <50057882+malawadd@users.noreply.github.com>
2022-08-14 14:25:56 +0300

    interface and optimaizations

---
+++ smart-contract/contracts/interfaces/ITasera.sol
@@ -0,0 +1,18 @@
+pragma solidity >=0.8.9 <0.9.0;
+pragma experimental ABIEncoderV2;
+
+interface IQuoter {
+    function estimateMaxSwapUniswapV3(
+        address _fromToken,
+        address _toToken,
+        uint256 _amount,
+        uint24 _poolFee
+    ) external view returns (uint256);
+
+    function estimateMinSwapUniswapV3(
+        address _fromToken,
+        address _toToken,
+        uint256 _amount,
+        uint24 _poolFee
+    ) external view returns (uint256);
+}
